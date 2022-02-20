f3cb2ba5bfb48e4aa130997a0b687aad0db427ea
Alexander <sachatarba@rambler.ru>
2022-02-20 20:30:51 +0300

    Корректировка тестов и код стайла

--- lab_01_05_05/main.c
+++ lab_01_05_05/main.c
@@ -36,6 +36,7 @@ void print_number(int number)
 
 bool input(int *number)
 {
+
     const char digits[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     char source[BUFF_SIZE];
     bool is_str_number = true;
