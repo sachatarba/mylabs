84921e0f365d5a26b322fa4e2adf9dad866b7c17
Alexander <sachatarba@rambler.ru>
2022-02-21 10:34:01 +0300

    Корректировка код стайла часть 4

--- lab_01_05_05/main.c
+++ lab_01_05_05/main.c
@@ -35,8 +35,10 @@ void print_number(int number)
 
 bool input(int *number)
 {
-    const char digits[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    
+    const char digits[10] = {
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 
+         };
+
     char source[BUFF_SIZE];
     bool is_str_number = true;
     bool is_el_digit = false;
