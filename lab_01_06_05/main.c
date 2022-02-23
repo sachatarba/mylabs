ae58371b1d42268814fd7bfacbbbffe415ef35b2
Alexander <sachatarba@rambler.ru>
2022-02-23 13:12:20 +0300

    Добавлено решение шестой задачи

---
+++ lab_01_06_05/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double vect_mul(double x1, double y1, double x2, double y2, double x3, double y3);
+int check_intesection(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4);
+
+int main(void)
+{
+    int exit_code = 0;
+    double xp, yp, xq, yq, xr, yr, xs, ys = 0;
+    if (scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &xp, &yp, &xq, &yq, &xr, &yr, &xs, &ys) == 8)
+    {
+        printf("%d", check_intesection(xp, yp, xq, yq, xr, yr, xs, ys));
+    }
+    else
+    {
+        exit_code = 1;
+    }
+    return exit_code;
+}
+
+double vect_mul(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+    return (x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1);
+}
+
+int check_intesection(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+{
+    int is_intersection = 0;
+    double dir1, dir2, dir3, dir4;
+    dir1 = vect_mul(x1, y1, x2, y2, x3, y3);
+    dir2 = vect_mul(x1, y1, x2, y2, x4, y4);
+    dir3 = vect_mul(x3, y3, x4, y4, x1, y1);
+    dir4 = vect_mul(x3, y3, x4, y4, x2, y2);
+    if ((dir1 * dir2 < 0) && (dir3 * dir4 < 0))
+    {
+        is_intersection = 1;
+    }
+    return is_intersection;
+}
+
