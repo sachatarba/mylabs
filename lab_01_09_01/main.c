16d6c3508983b704cdaed385a45bce33574fb889
Alexander <sachatarba@rambler.ru>
2022-02-27 15:07:41 +0300

    Выполнено вынесение в функцию расчет ответа, добавлена проверка ввода

--- lab_01_09_01/main.c
+++ lab_01_09_01/main.c
@@ -4,15 +4,29 @@
 
 #define ERR_OK 0
 #define ERR_IO 1
-#define ERR_INCORRECT_VALUES 2
 
+double calc_sum(size_t *current_iter);
 
 int main(void)
 {
     int exit_code = ERR_OK;
+    size_t current_iter = 1;
+    double sum = calc_sum(&current_iter);
+    if (current_iter == 1)
+    {
+        exit_code = ERR_IO;
+    }
+    if (!exit_code)
+    {
+        printf("%lf", sin(sum));
+    }
+    return exit_code;
+}
+
+double calc_sum(size_t *current_iter)
+{
     double x = 0;
     double sum = 0;
-    size_t current_iter = 1;
     while (1)
     {
         scanf("%lf", &x);
@@ -20,9 +34,8 @@ int main(void)
         {
             break;
         }
-        sum += sqrt(x / current_iter);
-        ++current_iter;
+        sum += sqrt(x / *current_iter);
+        ++(*current_iter);
     }
-    printf("%lf", sin(sum));
-    return exit_code;
+    return sum;
 }
