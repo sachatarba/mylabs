29b4ebdc2b0f4f513e648bfd9a8861e7077b5182
Alexander <sachatarba@rambler.ru>
2022-02-28 11:23:00 +0300

    Исправление по ревью третьей задачи

--- lab_01_03_01/main.c
+++ lab_01_03_01/main.c
@@ -2,7 +2,6 @@
 
 double calc_volume(double v1, double v2);
 double calc_temp(double v1, double t1, double v2, double t2);
-void show_answer(double v1, double t1, double v2, double t2);
 
 int main(void)
 {
@@ -11,12 +10,15 @@ int main(void)
 
     if (scanf("%lf%lf%lf%lf", &v1, &t1, &v2, &t2) == 4)
     {
-        show_answer(v1, t1, v2, t2);
+        double v = calc_volume(v1, v2);
+        double t = calc_temp(v1, t1, v2, t2);
+        printf("%lf %lf", v, t);
     }
     else
     {
         exit_code = 1;
     }
+
     return exit_code;
 }
 
@@ -29,11 +31,4 @@ double calc_temp(double v1, double t1, double v2, double t2)
 {
     double v = calc_volume(v1, v2);
     return (v1 * t1 + v2 * t2) / v;
-}
-
-void show_answer(double v1, double t1, double v2, double t2)
-{
-    double v = calc_volume(v1, v2);
-    double t = calc_temp(v1, t1, v2, t2);
-    printf("%lf %lf", v, t);
 }
