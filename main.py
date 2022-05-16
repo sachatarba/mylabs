f417bbad33e7b2f7381b5eea9ad5e9d0b7a8e244
Alexander Tarba <sachatarba@rambler.ru>
2022-05-17 01:19:25 +0300

    reformated code

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from math import pi
 
 
 class Point():
-    def __init__(self, x_in=None, y_in=None, event=None):
-        if event:
-            self.x, self.y = eval(str(event.pos())[19:])
+    def __init__(self, x_in=None, y_in=None, qt_point_event=None):
+        if qt_point_event:
+            self.x, self.y = eval(str(qt_point_event.pos())[19:])
 
-            self.point = event.pos()
+            self.point = qt_point_event.pos()
         else:
             self.x, self.y = x_in, y_in
-    # pass
 
-
-# p = Point(1, 2)
 class Triangle():
     def __init__(self, p1, p2, p3):
         self.point1 = p1
@@ -24,7 +21,6 @@ class Triangle():
         self.len_a = ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
         self.len_b = ((p1.x - p3.x) ** 2 + (p1.y - p3.y) ** 2) ** 0.5
         self.len_c = ((p2.x - p3.x) ** 2 + (p2.y - p3.y) ** 2) ** 0.5
-        self.demiPerimeter = (self.len_a + self.len_b + self.len_c) / 2
 
     def calcPerimeter(self):
         return self.len_a + self.len_b + self.len_c
@@ -74,28 +70,12 @@ class MainWidget(QWidget):
         self.setMinimumSize(QSize(745, 745))  # Устанавливаем размеры
         self.setWindowTitle("Geometry")  # Устанавливаем заголовок окна
         central_widget = PaintWidget()  # Создаём центральный виджет
-        # self.setCentralWidget(central_widget)  # Устанавливаем центральный виджет
         entry = Widget(central_widget)
-        # self.
-
         grid_layout = QGridLayout()  # Создаём QGridLayout
-        # central_widget.setLayout(grid_layout)  # Устанавливаем данное размещение в центральный виджет
         self.setLayout(grid_layout)
         grid_layout.addWidget(central_widget, 0, 0)
         grid_layout.addWidget(entry, 1, 0)
-        # lbl = QLabel("heeeeey", self)
-        # grid_layout.addWidget(lbl,1,0)
-        # lbl = QLabel("Проверка автодополнения", self)
-        # grid_layout.addWidget(lbl, 0, 0)
 
-        # Создаём поле ввода
-        # lineEdit = QLineEdit(self)
-        # strList = ['Python', 'PyQt5', 'Qt', 'Django', 'QML']  # Создаём список слов
-        # Создаём QCompleter, в который устанавливаем список, а также указатель на родителя
-        # completer = QCompleter(strList, lineEdit)
-        # lineEdit.setCompleter(completer)  # Устанавливает QCompleter в поле ввода
-        # grid_layout.addWidget(entry, 0, 0)  # Добавляем поле ввода в сетку
-        # lbl.move(-10, -10)
 
 
 class PaintWidget(QWidget):
@@ -108,23 +88,30 @@ class PaintWidget(QWidget):
         self.ansTriangle = None
         self.points = []
 
-    # def buttonClickedEvent(self, event):
+
+    def paintTriangle(self, painter, triangle):
+        pen = QPen(Qt.blue, 2, Qt.SolidLine)
+        painter.setPen(pen)
+        painter.drawLine(triangle.point1.x, triangle.point1.y, triangle.point2.x,
+                         triangle.point2.y)
+        painter.drawLine(triangle.point2.x, triangle.point2.y, triangle.point3.x,
+                         triangle.point3.y)
+        painter.drawLine(triangle.point1.x, triangle.point1.y, triangle.point3.x,
+                         triangle.point3.y)
 
     def mousePressEvent(self, event):
-        # print(event)
-        point = Point(event=event)
+        point = Point(qt_point_event=event)
         self.points.append(point)
 
         # Вызов перерисовки виджета
         self.update()
 
     def mouseReleaseEvent(self, event):
-        # self.point = None
         pass
 
     def paintEvent(self, event):
         super().paintEvent(event)
-        # print(event.pos())
+
         # Если нет
         if not self.points:
             return
@@ -134,27 +121,16 @@ class PaintWidget(QWidget):
 
         # Для рисования точки хватит setPen, но для других фигур (типо rect) понадобится setBrush
         painter.setPen(QPen(Qt.black, 5.0))
+
         try:
             # Рисование точки
             for point in self.points:
                 painter.drawPoint(point.x, point.y)
-                # painter.drawArc(point.x, point.y)
-                print(point.x, point.y)
 
+            # Если искомый треугольник существует, рисуем его
             if self.ansTriangle is not None:
-                pen = QPen(Qt.blue, 2, Qt.SolidLine)
-                # pen = QPen (QColor (255, 215, 0), 2, Qt.SolidLine) # Эффект такой же, как и в предыдущей строке кода
-                painter.setPen(pen)
-                painter.setBrush(QColor(255, 215, 0))
-                painter.drawLine(self.ansTriangle.point1.x, self.ansTriangle.point1.y, self.ansTriangle.point2.x,
-                                 self.ansTriangle.point2.y)
-                painter.drawLine(self.ansTriangle.point2.x, self.ansTriangle.point2.y, self.ansTriangle.point3.x,
-                                 self.ansTriangle.point3.y)
-                painter.drawLine(self.ansTriangle.point1.x, self.ansTriangle.point1.y, self.ansTriangle.point3.x,
-                                 self.ansTriangle.point3.y)
-                # painter.setBrush(QColor(255, 215, 0))
-                # painter.drawEllipse(*self.ansTriangle.calcCircumscribedCenter(), 2 * self.ansTriangle.calcR(), 2 * self.ansTriangle.calcR())
-                painter.drawEllipse(20, 10, 20, 20)
+                self.paintTriangle(painter, self.ansTriangle)
+
         except Exception:
             pass
 
@@ -188,13 +164,12 @@ class Widget(QWidget):
             self.cords = tuple(map(int, eval("({})".format(str(self.lineEdit.text())))))
         except Exception:
             self.lineEdit.setText("Ошибка ввода")
+
             return
-        print(*self.cords)
+
         point = Point(*self.cords)
-        print(point.x, point.y)
         self.paintWidget.points.append(point)
         self.paintWidget.update()
-        print(self.cords)
 
     def btnDelClicked(self):
         self.paintWidget.points = []
@@ -222,17 +197,12 @@ class Widget(QWidget):
             self.paintWidget.update()
         except Exception:
             pass
-        # qp = QPainter()
-        # qp.begin(self)
-        #
-        # qp.end()
+
 
 
 if __name__ == '__main__':
     app = QApplication([])
 
-    # w = Widget()
-    # w.show()
-    m = MainWidget()
-    m.show()
+    main = MainWidget()
+    main.show()
     app.exec()
